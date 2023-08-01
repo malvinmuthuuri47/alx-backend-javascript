7616aeb60124bf08f7df94e0f1f2e90109c7d87e
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-08-01 13:24:34 +0300

    Combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,20 @@
-export default function updateStudetGradeByCity(studentArr, city, newGrades) {
-	// We'll come back to that later
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-prototype-builtins */
+
+export default function updateStudentGradeByCity(studentArr, city, newGrade) {
+  return studentArr.filter((obj) => obj.location === city)
+    .map((student) => {
+      newGrade.map((studentGrade) => {
+        if (studentGrade.studentId === student.id) {
+          // eslint-disable-next-line no-param-reassign
+          student.grade = studentGrade.grade;
+        }
+
+        if (!student.hasOwnProperty('grade')) {
+          student.grade = 'N/A';
+        }
+        return student;
+      });
+      return student;
+    });
 }
