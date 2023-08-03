39b5467c4ea571a14040e6bd298d31859c30df8b
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-08-03 22:40:57 +0300

    Updated logic error in startString check

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString.length === 0 || startString === undefined) {
+  if (startString === undefined || startString.length === 0) {
     return '';
   }
 
