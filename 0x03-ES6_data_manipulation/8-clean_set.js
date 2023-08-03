8a0ce7318c642fcee06b2f42cea2335035744585
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-08-03 22:56:33 +0300

    Added missing check of when set is undefined

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,6 +2,9 @@ export default function cleanSet(set, startString) {
   if (startString === undefined || startString.length === 0) {
     return '';
   }
+  if (!set || !Array.isArray(set)) {
+    return '';
+  }
 
   const matchvals = Array.from(set).filter((value) => value.startsWith(startString));
   const finalset = matchvals.map((value) => value.substring(startString.length));
