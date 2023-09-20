41de915a5b7a7c2229120d6eb8a50e2e6b7236ed
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-09-20 16:29:16 +0000

    Updated solution to task 0

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,5 +1,8 @@
 function calculateNumber(a, b) {
-  return a + b;
+  const aRound = Math.round(a);
+  const bRound = Math.round(b);
+
+  return aRound + bRound;
 }
 
 module.exports = calculateNumber;
