0f2801f42121c3ffa2424c602838fe8b280b9d0c
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-09-20 16:44:30 +0000

    Updated solution for task 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,10 +1,16 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
+const { expect } = require('chai');
 
 describe('calculateNumber,', () => {
+	it('should test the rounding of a', () => {
+		const result = calculateNumber(4.2, 3.2);
+		expect(result).to.equal(7);
+	});
+
 	it('should add two positive numbers correctly', () => {
-		const result = calculateNumber(3, 5);
-		assert.strictEqual(result, 8);
+		const result = calculateNumber(3.8, 5);
+		assert.strictEqual(result, 9);
 	});
 
 	it('should add a positive and a negative number correctly', () => {
