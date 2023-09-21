6f15e6d999728f56be2ea809ea2dece855b12847
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-09-21 07:01:34 +0000

    Updated solution to task 3

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,32 +1,19 @@
 const sinon = require('sinon');
-const utils = require('./utils');
-const { sendPaymentRequestToAPI } = require('./3-payment');
+const { expect } = require('chai');
+const sendPaymentRequestToAPI = require('./3-payment');
+const Utils = require('./utils');
 
-describe('sendPaymentRequestToApi', () => {
-  let calculateNumberSpy;
 
-  beforeEach(() => {
-    // Create a spy for the utils.calculateNumber function
-    calculateNumberSpy = sinon.spy(utils, 'calculateNumber');
-  });
+describe('sendPaymentRequestToAPI', () => {
+  it('should call utils.calculateNumber with correct arguments', () => {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    const totalAmount = 100;
+    const totalShipping = 20;
+    sendPaymentRequestToAPI(totalAmount, totalShipping);
+    expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
+    //expect(calculateNumberSpy).to.have.been.calledWith('SUM', 100, 20);
 
-  afterEach(() => {
-    // Restore the spy after each test to prevent side effects
+    // Reastore stub to its original state
     calculateNumberSpy.restore();
-  });
-
-  it('should call utils.calculateNumber with type "SUM", totalAmount, and totalShipping', () => {
-    sendPaymentRequestToAPI(100, 20);
-    // Check if utils.calculateNumber was called with expected args
-    sinon.assert.calledWithExactly(calculateNumberSpy, 'SUM', 100, 20);
-  });
-
-  it('should return the result obtained from utils.calculateNumber', () => {
-    // Set up the spy to return a specific value when called
-    calculateNumberSpy.returns(120);
-    const result = sendPaymentRequestToAPI(100, 20);
-
-    // Check if the result matches the expected value
-    sinon.assert.match(result, 120);
   });
 });
