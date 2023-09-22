f6877ffa025d714ebb05dfe71694b47b0883ac05
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-09-22 10:02:13 +0000

    Updated solution for task 3

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,19 +1,28 @@
 const sinon = require('sinon');
-const { expect } = require('chai');
+const assert = require('assert');
 const sendPaymentRequestToAPI = require('./3-payment');
-const Utils = require('./utils');
+const utils = require('./utils');
 
+describe('sendPaymentequestToAPI', () => {
+    before(() => {
+        calculateNumberSpy = sinon.spy(utils, 'calculateNumber');
+    });
 
-describe('sendPaymentRequestToAPI', () => {
-  it('should call utils.calculateNumber with correct arguments', () => {
-    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    after(() => {
+        utils.calculateNumber.restore();
+    })
+  
+  it('should call utils.calculateNumber with correct args', () => {
     const totalAmount = 100;
     const totalShipping = 20;
     sendPaymentRequestToAPI(totalAmount, totalShipping);
-    expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
-    //expect(calculateNumberSpy).to.have.been.calledWith('SUM', 100, 20);
 
-    // Reastore stub to its original state
-    calculateNumberSpy.restore();
+    const firstCall = calculateNumberSpy.getCall(0);
+
+    const args = firstCall.args;
+
+    console.log('Arguments received by the spy: ', args);
+
+    assert(calculateNumberSpy.calledWith('SUM', totalAmount, totalShipping));
   });
 });
